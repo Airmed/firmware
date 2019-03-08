fccc6a276100795639f7ec7a793803b0fe66b07b
Chris Morroni <chmorroni@gmail.com>
2019-03-07 23:05:59 -0700

    added IT-03 procedure

---
+++ apotech/source/tests/it-03.c
@@ -0,0 +1,36 @@
+
+#include "drum.h"
+#include "gpio.h"
+
+extern void hardware_init();
+
+int main(void)
+{
+    drum_hoppers_e hopper_next = DRUM_HOPPER_0;
+
+    hardware_init();
+
+    while (true)
+    {
+        while (button_patient_get_status() == false);
+
+        drum_set_hopper(hopper_next);
+        switch (hopper_next)
+        {
+            case DRUM_HOPPER_0:
+                hopper_next = DRUM_HOPPER_1;
+                break;
+            case DRUM_HOPPER_1:
+                hopper_next = DRUM_HOPPER_2;
+                break;
+            case DRUM_HOPPER_2:
+                hopper_next = DRUM_HOPPER_3;
+                break;
+            case DRUM_HOPPER_3:
+                hopper_next = DRUM_HOPPER_0;
+                break;
+        }
+    }
+
+    return 0;
+}
